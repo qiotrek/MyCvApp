4f5fc0dcd357260e23a9c5f45bbb50b5cc01bc04
Piotr Plazinski <piotrplazinski@gmail.com>
2026-01-22 01:09:35 +0100

    cv replacement

--- src/Hooks/CVDownload.tsx
+++ src/Hooks/CVDownload.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function CVDownload() {
 
     const handleDownload =async () => {
-        const filePath = '/assets/CV.docx';
+    const filePath = '/assets/PiotrPłazińskiCV.docx';
     
         try {
           const response = await fetch(process.env.PUBLIC_URL + filePath);
@@ -11,7 +11,7 @@ export default function CVDownload() {
     
           const downloadLink = document.createElement('a');
           downloadLink.href = window.URL.createObjectURL(blob);
-          downloadLink.download = 'myCv.docx';
+          downloadLink.download = 'PiotrPłazińskiCV.docx';
     
           downloadLink.type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
     
@@ -49,7 +49,7 @@ export default function CVDownload() {
                                     <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
                                     </svg>
                                     <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                                        <span className="truncate font-medium text-gray-400">CV.docx</span>
+                                        <span className="truncate font-medium text-gray-400">PiotrPłazińskiCV.docx</span>
                                         <span className="flex-shrink-0 text-gray-400">4.5mb</span>
                                     </div>
                                 </div>
